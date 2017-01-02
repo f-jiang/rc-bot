5539e3712c2ae1be2a683104eaabda64d363aa21
Feilan Jiang <fjiang120@gmail.com>
2017-01-01 20:27:21 -0500

    Revise comments and variable/function names

--- bot/main/main.ino
+++ bot/main/main.ino
@@ -1,33 +1,32 @@
-#define M_L_F 5 // IN1 Blue
-#define M_L_R 4 // IN2 Green
-#define M_R_F 6 // IN3 Yellow
-#define M_R_R 7 // IN4 Orange
-#define SERVO_OUT 9 // Servo Motor Output
+#define M_L_F 5 // IN1
+#define M_L_R 4 // IN2
+#define M_R_F 6 // IN3
+#define M_R_R 7 // IN4
+#define SERVO 9
 
-#define echoPin 2 // Echo Pin
-#define trigPin 3 // Trigger Pin
-#define LEDPin 13 // Onboard LED
+#define ULTRA_ECHO 2
+#define ULTRA_TRIG 3
+#define LED 13
 
 #include <Servo.h>
 
 enum DriveState { REV, OFF, FWD };
 
-Servo myservo;  // create servo object to control a servo
-int pos = 0;    // variable to store the servo position
+Servo ultraServo;
 
-int maximumRange = 200; // Maximum range needed
-int minimumRange = 0; // Minimum range needed
-long distance; // Duration used to calculate distance
+int maximumRange = 200;
+int minimumRange = 0;
+long distance;
 
 void setup() {
     Serial.begin (9600);
 
     // initialize ultrasonic sensor + servo mount
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
-    pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
-    myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-    myservo.write(90);
+    pinMode(ULTRA_TRIG, OUTPUT);
+    pinMode(ULTRA_ECHO, INPUT);
+    pinMode(LED, OUTPUT);
+    ultraServo.attach(9);
+    ultraServo.write(90);
 
     // Initialize Motor Pins
     pinMode(M_L_F, OUTPUT);
@@ -43,20 +42,18 @@ void setup() {
     delay(1000);
 }
 
-long get_dist() {
+long getDist() {
     long duration;
-    /* The following trigPin/echoPin cycle is used to determine the
-    distance of the nearest object by bouncing soundwaves off of it. */ 
-    digitalWrite(trigPin, LOW); 
+    digitalWrite(ULTRA_TRIG, LOW); 
     delayMicroseconds(2); 
 
-    digitalWrite(trigPin, HIGH);
+    digitalWrite(ULTRA_TRIG, HIGH);
     delayMicroseconds(10); 
 
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
+    digitalWrite(ULTRA_TRIG, LOW);
+    duration = pulseIn(ULTRA_ECHO, HIGH);
 
-    //Calculate the distance (in cm) based on the speed of sound.
+    // dist in cm
     return duration / 58.2;
 }
 
@@ -81,28 +78,33 @@ void drive(DriveState left, DriveState right) {
     }
 }
 
-void servo_write(int angle) {
-    myservo.write(angle + 90);
+void servoWrite(int angle) {
+    ultraServo.write(angle + 90);
 }
 
 void loop() {
-    distance = get_dist();
+    distance = getDist();
 
     if (distance >= maximumRange || distance <= minimumRange) {
-        /* Send a negative number to computer and Turn LED ON 
-        to indicate "out of range" */
         Serial.println("-1");
-        digitalWrite(LEDPin, HIGH); 
+        digitalWrite(LED, HIGH); 
     } else {
-        /* Send the distance to the computer using Serial protocol, and
-        turn LED OFF to indicate successful reading. */
         Serial.println(distance);
-        digitalWrite(LEDPin, LOW); 
+        digitalWrite(LED, LOW); 
     }
 
     delay(50);
 
-    servo_write(0);
+    servoWrite(0);
+
+    drive(FWD, FWD);
+    delay(1000);
+    drive(OFF, OFF);
+    delay(1000);
+    drive(REV, REV);
+    delay(1000);
+    drive(OFF, OFF);
+    delay(2000);
 }
 
 
