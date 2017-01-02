1801550f33089d2ec964f7b944d714e1b5671a09
Feilan Jiang <fjiang120@gmail.com>
2017-01-01 19:32:13 -0500

    Merge ultrasonic and full robot code

--- bot/main/main.ino
+++ bot/main/main.ino
@@ -9,11 +9,22 @@
 #define OFF 0
 #define REV -1
 
+#define echoPin 2 // Echo Pin
+#define trigPin 3 // Trigger Pin
+#define LEDPin 13 // Onboard LED
+
 
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
+
+
+int maximumRange = 200; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
+
 
 // Prototypes
 void motor_left(int state);
@@ -25,6 +36,11 @@ void motor_stop();
 
 void setup() 
 {
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
+  
   Serial.begin(9600); // Serial Monitor Rate Initialization  
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 
@@ -46,6 +62,35 @@ void setup()
 }
 
 void loop() {
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ if (distance >= maximumRange || distance <= minimumRange) {
+   /* Send a negative number to computer and Turn LED ON 
+   to indicate "out of range" */
+   Serial.println("-1");
+   digitalWrite(LEDPin, HIGH); 
+ } else {
+   /* Send the distance to the computer using Serial protocol, and
+   turn LED OFF to indicate successful reading. */
+   Serial.println(distance);
+   digitalWrite(LEDPin, LOW); 
+ }
+ 
+ //Delay 50ms before next reading.
+ delay(50);
+  
   servo_write(0);
 
   motor_fwd();
